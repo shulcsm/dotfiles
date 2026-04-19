ec6718e24819c0cecd0b87b93301fee6d4533176
Mārtiņš Šulcs <shulcsm@gmail.com>
2026-04-19 22:26:38 +0300

    Starship theme. Format json files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -615,6 +615,8 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        json = { 'biome', 'prettierd', stop_after_first = true },
+        jsonc = { 'biome', 'prettierd', stop_after_first = true },
       },
     },
   },
